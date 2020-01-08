c0af499d1fcfc5398ac4ef46c5d62c20d0130e99
Diogo Ribeiro <josediogoccbr@gmail.com>
2020-01-08 10:54:28 +0000

    add navbar styling for small screens

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+
+//href="javascript:void(0);" onclick="myFunction()">
+function showBurger() {
+  var x = document.getElementById("menu__links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
